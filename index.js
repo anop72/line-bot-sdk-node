ba3af90ad0bf40f23b05c99a43dfcc62f8017b18
ak <anop72@gmail.com>
2016-10-02 16:29:46 +0700

    add configuration

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const
   bodyParser = require('body-parser'),
+  config = require('config'),
+  crypto = require('crypto'),
   express = require('express'),
   https = require('https'),
   request = require('request');
@@ -10,6 +12,23 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+
+const CHANNEL_ID = (process.env.CHANNEL_ID) ?
+  process.env.CHANNEL_ID :
+  config.get('channelId');
+
+const CHANNEL_SECRET = (process.env.CHANNEL_SECRET) ?
+  process.env.CHANNEL_SECRET :
+  config.get('channelSecret');
+
+if (!(CHANNEL_ID && CHANNEL_SECRET)) {
+  console.error("Missing config values");
+  process.exit(1);
+}
+
+app.get('/webhook', function(req, res) {
+  res.sendStatus(200);
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
